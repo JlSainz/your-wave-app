60e336203dd681e6b329822af6fba2d3dbd8ba8f
Pepe Sainz <psainz2593@gmail.com>
2019-10-06 20:21:54 +0200

    Login done

---
+++ client/src/components/contents/Contents.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Contents extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          OK!
+        </p>
+      </div>
+    );
+  }
+}
+export default Contents;
