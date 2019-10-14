3d52286753c24cc8dc18790d975f00335f558275
Pepe Sainz <psainz2593@gmail.com>
2019-10-14 17:46:37 +0200

    heroku

---
+++ server/public/precache-manifest.65da586c835829e9d0495861c0837266.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd66f613a1e28ffbe13ff1b02452fbba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a63543bdd4f27a1f0bd6",
+    "url": "/static/css/main.c121c2a7.chunk.css"
+  },
+  {
+    "revision": "7384c30396159055e5a8",
+    "url": "/static/js/2.12de98f7.chunk.js"
+  },
+  {
+    "revision": "a63543bdd4f27a1f0bd6",
+    "url": "/static/js/main.c58289a3.chunk.js"
+  },
+  {
+    "revision": "8c676b4634e3c9c46b49",
+    "url": "/static/js/runtime-main.d47ac574.js"
+  }
+]);
