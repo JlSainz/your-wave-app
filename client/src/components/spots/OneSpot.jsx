980fb10ef7696e41d2a2365d56d9e9936cff2e5f
Pepe Sainz <psainz2593@gmail.com>
2019-10-14 14:23:34 +0200

    en proceso

---
+++ client/src/components/spots/OneSpot.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import Gmaps from "./../gmaps/Gmaps";
+
+export default class OneSpot extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      spot: {}
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    state.spot = props.spot;
+    // console.log(props.spot.location.coordinates[0]);
+  }
+
+  getMeteo = () => {
+    console.log("GETTING FORECAST...");
+    // const lng = props.spot.location.coordinates[0];
+    // const lat = props.spot.location.coordinates[1];
+    const lng = 115.838236;
+    const lat = -8.739984;
+    // const params = "swellHeight, waveHeight,seaLevel, windDirection, windSpeed";
+    // "time,airTemperature,swellDirection, swellHeight, swellPeriod, waterTemperature, waveDirection, waveHeight, wavePeriod,seaLevel, windDirection, windSpeed";
+
+    fetch(`https://api.stormglass.io/v1/weather/point?lat=${lat}&lng=${lng}`, {
+      headers: {
+        Authorization:
+          "807b335c-e8f5-11e9-80bf-0242ac130004-807b347e-e8f5-11e9-80bf-0242ac130004"
+      }
+    })
+      .then(response => response.json())
+      .then(jsonData => {
+        // Do something with response data.
+        console.log(jsonData);
+      });
+  };
+
+  render() {
+    const { spot } = this.state;
+    return (
+      <div className="spot">
+        <h1>Name: {spot.name}</h1>
+        <img src={spot.imageURL} alt="Photo" />
+        <p>Country: {spot.country}</p>
+        <ul>
+          <li>Nearby: {spot.nearby}</li>
+          <li>Consistence: {spot.consistence}</li>
+          <li>Commment: {spot.comment.text}</li>
+          <li>Rating: {spot.comment.rating}</li>
+          <li>Wave form: {spot.wave_form}</li>
+          <li>Wave direction: {spot.wave_direction}</li>
+          <li>Weather: {spot.weather}</li>
+          <li>Period: {spot.period}</li>
+          <li>Break type: {spot.break_type}</li>
+          <li>Level: {spot.level}</li>
+          <li>Vibe: {spot.vibe}</li>
+          <li>Lng: {spot.location.coordinates[0]}</li>
+          <li>Lat: {spot.location.coordinates[1]}</li>
+        </ul>
+        <Gmaps coordinates={spot.location.coordinates} />
+        <button onClick={this.getMeteo}>Get Forecast!</button>
+      </div>
+    );
+  }
+}
--- client/src/components/spots/Spots.jsx
+++ client/src/components/spots/Spots.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import SpotsService from "./../services/SpotsService";
-import Gmaps from "./../gmaps/Gmaps";
+// import SpotsService from "./../services/SpotsService";
 import "./Spots.css";
+import OneSpot from "./OneSpot";
 
 export default class Spots extends Component {
   constructor(props) {
@@ -14,29 +14,7 @@ export default class Spots extends Component {
     return (
       <div className="container-spot">
         {this.props.spots.map((spot, index) => {
-          return (
-            <div className="spot" key={index}>
-              <h1>Name: {spot.name}</h1>
-              <img src={spot.imageURL} alt="Photo" />
-              <p>Country: {spot.country}</p>
-              <ul>
-                <li>Nearby: {spot.nearby}</li>
-                <li>Consistence: {spot.consistence}</li>
-                <li>Commment: {spot.comment.text}</li>
-                <li>Rating: {spot.comment.rating}</li>
-                <li>Wave form: {spot.wave_form}</li>
-                <li>Wave direction: {spot.wave_direction}</li>
-                <li>Weather: {spot.weather}</li>
-                <li>Period: {spot.period}</li>
-                <li>Break type: {spot.break_type}</li>
-                <li>Level: {spot.level}</li>
-                <li>Vibe: {spot.vibe}</li>
-                <li>Coordinates: {spot.location.coordinates}</li>
-              </ul>
-              <Gmaps coordinates={spot.location.coordinates} />
-              <button>Get Forecast!</button>
-            </div>
-          );
+          return <OneSpot key={index} spot={spot} />;
         })}
       </div>
     );
